08a2ebd756b96bc6f599b0c4ac4235361637b81c
Flexandr <aleksandrberbat@gmail.com>
2020-09-03 15:07:07 +0300

    task 04.09 v0.2

--- Task (04.09)/js/main.js
+++ Task (04.09)/js/main.js
@@ -2,6 +2,22 @@
 let a = 3;
 let b = 4;
 
+
 code.style.backgroundColor = "yellow";
 document.getElementById("code").innerHTML = `${b << ++a}`;
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let password = "1234";
+
+function openDoor()
+{
+    let passwordInput = document.getElementById("password").value;
+    console.log(passwordInput);
+
+    if(passwordInput == password)
+    {
+        document.getElementById("door_stat").innerHTML = "opened";
+        document.getElementById("door_res").src = "res/door_opened.png"
+    }
+}
