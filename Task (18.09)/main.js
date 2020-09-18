83409479ef598e1095f83622ef7b21f050579819
Flexandr <aleksandrberbat@gmail.com>
2020-09-18 16:28:18 +0300

    task 18.09 v0.3

--- Task (18.09)/main.js
+++ Task (18.09)/main.js
@@ -35,41 +35,23 @@ function createCar(carNumber, bodyColorInd, transmissionInd, conditionerInd, int
 let cars = [];
 
 
-
+let pad = "0000";
 let carNumb = 0;
 
-for (let b = 0; b < carAllConf.bodyColor.length; b++) 
+for (let bodyColorIter = 0; bodyColorIter < carAllConf.bodyColor.length; bodyColorIter++) 
 {
-    for (let c = 0; c < carAllConf.transmission.length; c++) 
+    for (let transmissionIter = 0; transmissionIter < carAllConf.transmission.length; transmissionIter++) 
     {
-        for (let e = 0; e < carAllConf.conditioner.length; e++)
+        for (let conditionerIter = 0; conditionerIter < carAllConf.conditioner.length; conditionerIter++)
         {
-            for(let f = 0; f < carAllConf.interior.length; f++)
+            for(let interiorIter = 0; interiorIter < carAllConf.interior.length; interiorIter++)
             {
 
-                if (carNumb < 10) 
-                {
-                cars[carNumb] = createCar(`000${carNumb}`, b, c, e, f);
-                } else if (carNumb >= 10 && carNumb < 100) 
-                {
-
-                cars[carNumb] = createCar(`00${carNumb}`, b, c, e, f);
-                } else if (carNumb >= 100 && carNumb < 1000) 
-                {
-
-                cars[carNumb] = createCar(`0${carNumb}`, b, c, e, f);
-                } else if (carNumb >= 1000 && carNumb < 10000) 
-                {
-
-                cars[carNumb] = createCar(`${carNumb}`, b, c, e, f);
-                }
+                cars[carNumb] = createCar((pad+carNumb).slice(-pad.length), bodyColorIter, transmissionIter, conditionerIter, interiorIter);
                 carNumb++;
             }
-
         }
-
     }
-
 }
 
 console.log(cars);
@@ -84,50 +66,13 @@ console.log("\n\nTask 2:");
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function changeArray(arr) {
+function newArray(arr) 
+{
     let arrTemp = [];
     arrTemp.length = arr.length;
 
     arrTemp[0] = arr[0];
-    arrTemp[arrTemp.length - 1] = arr[arr.length - 1];
-
-    let firstEvenNum = true//bool для проверки, первое ли это четное число из пары или второе
-    let firstOddNum = true//bool для проверки, первое ли это нечетное число из пары или второе (кроме 5)
-
-    for (let i = 1; i < arr.length - 1; i++) {
-
-        if (arr[i] % 2 == 0) {
-
-            if (firstEvenNum == true) {
-                arrTemp[i] = arr[i + 2];
-                arrTemp[i + 2] = arr[i];
-                firstEvenNum = false;
-            }
-            else {
-                firstEvenNum = true;
-            }
-        }
-        else if (arr[i] % 5 == 0) {
-
-            arrTemp[i] = arr[i];
-        }
-        else if (arr[i] % 2 != 0) {
-
-            if (firstOddNum == true) {
-                arrTemp[i] = arr[i + 4];
-                arrTemp[i + 4] = arr[i];
-                firstOddNum = false
-            }
-            else {
-                firstOddNum = true;
-            }
-        }
-
-
-    }
-
-
-    return arrTemp;
 }
 
-console.table(changeArray(arr));
+
+console.table(newArray(arr));
