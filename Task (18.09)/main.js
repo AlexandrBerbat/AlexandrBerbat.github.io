0c6081d801da4cb6695c59f77ed28c5aca1d3b0a
Flexandr <aleksandrberbat@gmail.com>
2020-09-18 14:04:05 +0300

    task 18.09 v0.2

--- Task (18.09)/main.js
+++ Task (18.09)/main.js
@@ -1,14 +1,13 @@
 console.log("Task 1:");
 
-function randomInt(min, max)
-{
+function randomInt(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 }
 
 const carAllConf =
 {
-    bodyColor: ["Yellow", "Green", "Red", "Blue", "Orange" ],
+    bodyColor: ["Yellow", "Green", "Red", "Blue", "Orange"],
     transmission: ["Automatical", "Mechanical"],
     conditioner: ["True", "False"],
     interior: ["Leather", "Textile", "Combined"],
@@ -16,15 +15,15 @@ const carAllConf =
 
 
 
-function createCar(carNumber)
-{
+function createCar(carNumber, bodyColorInd, transmissionInd, conditionerInd, interiorInd) {
     let carTemp = {};
     carTemp.carNumber = carNumber;
 
-    carTemp.bodyColor = carAllConf.bodyColor[randomInt(0, carAllConf.bodyColor.length-1)];
-    carTemp.transmission = carAllConf.transmission[randomInt(0, carAllConf.transmission.length-1)];
-    carTemp.conditioner = carAllConf.conditioner[randomInt(0, carAllConf.conditioner.length-1)];
-    carTemp.interior = carAllConf.interior[randomInt(0, carAllConf.interior.length-1)];
+
+    carTemp.bodyColor = carAllConf.bodyColor[bodyColorInd];
+    carTemp.transmission = carAllConf.transmission[transmissionInd];
+    carTemp.conditioner = carAllConf.conditioner[conditionerInd];
+    carTemp.interior = carAllConf.interior[interiorInd];
 
     carTemp.confCode = carTemp.carNumber + carTemp.bodyColor[0] + carTemp.transmission[0] + carTemp.conditioner[0] + carTemp.interior[0];
 
@@ -32,28 +31,49 @@ function createCar(carNumber)
 };
 
 
-let carAmount = 10;
-let cars = [];
-cars.length = carAmount;
 
-for(let i = 0; i < carAmount; i ++)
+let cars = [];
+
+
+
+let carNumb = 0;
+
+for (let b = 0; b < carAllConf.bodyColor.length; b++) 
 {
-    if(i<10)
+    for (let c = 0; c < carAllConf.transmission.length; c++) 
     {
-        cars[i] = createCar(`000${i}`);
-    }else if(i>= 10 && i <100)
-    {
-        cars[i] = createCar(`00${i}`);
-    }else if(i>= 100 && i <1000)
-    {
-        cars[i] = createCar(`0${i}`);
-    }else if(i>= 1000 && i <10000)
-    {
-        cars[i] = createCar(`${i}`);
+        for (let e = 0; e < carAllConf.conditioner.length; e++)
+        {
+            for(let f = 0; f < carAllConf.interior.length; f++)
+            {
+
+                if (carNumb < 10) 
+                {
+                cars[carNumb] = createCar(`000${carNumb}`, b, c, e, f);
+                } else if (carNumb >= 10 && carNumb < 100) 
+                {
+
+                cars[carNumb] = createCar(`00${carNumb}`, b, c, e, f);
+                } else if (carNumb >= 100 && carNumb < 1000) 
+                {
+
+                cars[carNumb] = createCar(`0${carNumb}`, b, c, e, f);
+                } else if (carNumb >= 1000 && carNumb < 10000) 
+                {
+
+                cars[carNumb] = createCar(`${carNumb}`, b, c, e, f);
+                }
+                carNumb++;
+            }
+
+        }
+
     }
+
 }
 
 console.log(cars);
+
 
 
 
@@ -64,8 +84,7 @@ console.log("\n\nTask 2:");
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function changeArray(arr) 
-{
+function changeArray(arr) {
     let arrTemp = [];
     arrTemp.length = arr.length;
 
@@ -77,36 +96,29 @@ function changeArray(arr)
 
     for (let i = 1; i < arr.length - 1; i++) {
 
-        if (arr[i] % 2 == 0) 
-        {
+        if (arr[i] % 2 == 0) {
 
-            if (firstEvenNum == true) 
-            {
+            if (firstEvenNum == true) {
                 arrTemp[i] = arr[i + 2];
                 arrTemp[i + 2] = arr[i];
                 firstEvenNum = false;
-            } 
-            else 
-            {
+            }
+            else {
                 firstEvenNum = true;
             }
-        } 
-        else if (arr[i] % 5 == 0) 
-        {
+        }
+        else if (arr[i] % 5 == 0) {
 
             arrTemp[i] = arr[i];
-        } 
-        else if (arr[i] % 2 != 0) 
-        {
+        }
+        else if (arr[i] % 2 != 0) {
 
-            if (firstOddNum == true) 
-            {
+            if (firstOddNum == true) {
                 arrTemp[i] = arr[i + 4];
                 arrTemp[i + 4] = arr[i];
                 firstOddNum = false
             }
-            else 
-            {
+            else {
                 firstOddNum = true;
             }
         }
