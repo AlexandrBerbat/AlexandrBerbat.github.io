b75a20df7ed53ca4d96c4545b9e736e141100be6
Flexandr <aleksandrberbat@gmail.com>
2020-10-06 15:05:29 +0300

    task 03.10 classwork

--- Task (03.10) Classwork/main.js
+++ Task (03.10) Classwork/main.js
@@ -1,49 +1,77 @@
-const timeInputEl = document.querySelector(".timerInput");
-const startBtnEl = document.querySelector(".startBtn");
-const progressIconEl = document.querySelector(".progressIcon");
-const timeLeftEl = document.querySelector(".timeLeft");
+// const timeInputEl = document.querySelector(".timerInput");
+// const startBtnEl = document.querySelector(".startBtn");
+// const progressIconEl = document.querySelector(".progressIcon");
+// const timeLeftEl = document.querySelector(".timeLeft");
 
-let pad = "00";
-
-function TimeToSec(time)
-{
-    let timeArr = time.split(":");
-    return Number(timeArr[0])*60 + Number(timeArr[1]);
-}
-
-// let timeLeftVal = TimeToSec(timeInputEl.value);
-// console.log(new Date(+timeLeftVal * 1000));
+// let pad = "00";
 
 
-const startTimer = () => {
-    progressIconEl.classList.add("start");
+// function TimeToSec(time)//часы-минуты в секунды
+// {
+//     let timeArr = time.split(":");
+//     return Number(timeArr[0])*60 + Number(timeArr[1]);
+// }
 
-    let timeLeftVal = new Date(+TimeToSec(timeInputEl.value) * 1000 * 60);
+// // let timeLeftVal = TimeToSec(timeInputEl.value);
+// // console.log(new Date(+timeLeftVal * 1000));
 
-    let timerOutput = () => {
+
+// const startTimer = () => {
+//     progressIconEl.classList.add("start");//css
+
+//     console.log("timeInputEl.value " + timeInputEl.value);
+
+//     let timeLeftVal = new Date(TimeToSec(timeInputEl.value) * 1000 * 60);
+
+//     console.log("timeLeftVal    :" + timeLeftVal);
+
+//     // let timeToStop = new Date();
+//     // timeToStop = moment(timeToStop).add(timeLeftVal);
+
+//     // console.log("timeToStop" + timeToStop._d);
+
+//     let timerOutput = () => {
         
-        timeLeftEl.classList.remove("stopped");
-        startBtnEl.disabled = true;
+//         //css
+//         timeLeftEl.classList.remove("stopped");
+//         startBtnEl.disabled = true;
+//         //css2
 
-        timeLeftEl.innerHTML = `${(pad+timeLeftVal.getMinutes()).slice(-pad.length)}:${(pad+timeLeftVal.getSeconds()).slice(-pad.length)}`;
 
-        let timeLeftValTemp = new Date(timeLeftVal - 1000);
-        timeLeftVal = timeLeftValTemp;
 
-        console.log(timeLeftVal);
-        if(moment(timeLeftVal).isSame(new Date(0)-1000))
-        {
-            progressIconEl.classList.remove("start");
-            timeLeftEl.classList.add("stopped");
-            startBtnEl.disabled = false;
-            clearInterval(timerId);
-        }
+//         timeLeftEl.innerHTML = `${(pad+timeLeftVal.getMinutes()).slice(-pad.length)}:${(pad+timeLeftVal.getSeconds()).slice(-pad.length)}`;
+
+//         let timeLeftValTemp = new Date(+timeLeftVal - 1000 - 10800 * 1000);
+//         timeLeftVal = timeLeftValTemp;
+
+        
+
+//         console.log(timeLeftVal); //время до истечения таймера
+
+//         // console.log(new Date().getTime());
+//         // console.log(timeToStop.d_);
+
+//         let tempNow = new Date();
+//         console.log("111111111!!!!!!" + tempNow.getHours());
+//         console.log("TimeLeftVal hourrs  :" + timeLeftVal.getHours());
+
+//         // if(new Date().getTime() == new Date(timeToStop._d.getTime()))
+//         if(tempNow.getHours() === timeLeftVal.getHours() && tempNow.getMinutes() === timeLeftVal.getMinutes())
+//         {
+//             //css
+//             progressIconEl.classList.remove("start");
+//             timeLeftEl.classList.add("stopped");
+//             //css
+
+//             startBtnEl.disabled = false;
+//             clearInterval(timerId);
+//         }
     
-    };
+//     };
 
 
-    let timerId = setInterval(timerOutput, 1000);
-}
+//     let timerId = setInterval(timerOutput, 1000);
+// }
 
-startBtnEl.addEventListener("click",startTimer);
+// startBtnEl.addEventListener("click",startTimer);
 
---
+++ Task (03.10) Classwork/main2.js
@@ -0,0 +1,58 @@
+const timeInputEl = document.querySelector(".timerInput");
+const startBtnEl = document.querySelector(".startBtn");
+const progressIconEl = document.querySelector(".progressIcon");
+const timeLeftEl = document.querySelector(".timeLeft");
+
+let pad = "00";
+
+function TimeToSec(time)//часы-минуты в секунды
+{
+    let timeArr = time.split(":");
+    return Number(timeArr[0]) * 60 + Number(timeArr[1]);
+}
+
+
+
+const startTimer = () => {
+
+    progressIconEl.classList.add("start");//css
+
+
+    let timeInputTemp = new Date(TimeToSec(timeInputEl.value) * 1000 * 60 - 10800 * 1000);
+    let tempNow2 = new Date();
+
+    let timeLeft = new Date((TimeToSec(`${timeInputTemp.getHours()}:${timeInputTemp.getMinutes()}`)-TimeToSec(`${tempNow2.getHours()}:${tempNow2.getMinutes()}`)) * 1000 * 60);
+
+
+    let timeToStop = new Date(moment(timeLeft.getTime()).add(tempNow2.getTime()));
+   
+    timeLeft = moment(timeLeft);
+
+    const timerOutput = () => {
+
+        //css
+        timeLeftEl.classList.remove("stopped");
+        startBtnEl.disabled = true;
+        //css2
+        let timeTempNow = new Date();
+
+        timeLeftEl.innerHTML = `${(pad+timeLeft._d.getMinutes()).slice(-pad.length)}:${(pad+timeLeft._d.getSeconds()).slice(-pad.length)}`;
+        timeLeft = moment(timeLeft).subtract(1, "second");
+
+
+        if (timeLeft < 0) {
+            //css
+            progressIconEl.classList.remove("start");
+            timeLeftEl.classList.add("stopped");
+            //css
+
+            startBtnEl.disabled = false;
+            clearInterval(timerId);
+        }
+
+    }
+
+    let timerId = setInterval(timerOutput, 1000);
+}
+
+startBtnEl.addEventListener("click", startTimer);
