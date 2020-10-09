c6da09964be2e4930e84a6f1025aaad4bcb9c997
Flexandr <aleksandrberbat@gmail.com>
2020-10-09 19:54:36 +0300

    task 09.10 classwork v0.2

--- Task (09.10) Classwork/js/main.js
+++ Task (09.10) Classwork/js/main.js
@@ -2,16 +2,77 @@ const fieldLength = 5;
 
 let fieldEl = document.querySelector(".field");
 
-let fieldArr = [];
+function createArray() {//создание двумерного массива
+    let array = [];
 
-for(let i = 0; i < fieldLength; i++)
-{
-    let tempArr = [];
-    for(let a = 0; a < fieldLength; a++)
-    {
-        tempArr[a] = a+1;
+    for (let i = 0; i < fieldLength; i++) {
+        let tempArr = [];
+        for (let a = 0; a < fieldLength; a++) {
+            tempArr[a] = a + 1;
+        }
+        array[i] = tempArr;
     }
-    fieldArr[i] = tempArr;
+    return array;
 }
 
-console.table(fieldArr);
+let fieldArr = createArray();
+
+console.log(fieldArr);
+
+
+function clearArray(array) {//очистка двумерного массива
+    for (let i = 0; i < array.length; i++) {
+        for (let a = 0; a < array.length; a++) {
+            array[i][a] = null;
+        }
+    }
+}
+// clearArray(fieldArr);
+
+
+
+function findFilledCells(array)//количество ненулевых(непустых) ячеек массива
+{
+    let tempCells = 0;
+    for (let i = 0; i < array.length; i++) {
+        for (let a = 0; a < array.length; a++) {
+            if (array[i][a]) {
+                tempCells++;
+            }
+        }
+    }
+
+    return tempCells;
+}
+
+let emptyCells = findFilledCells(fieldArr);
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+function fillUpField(fieldEl, array)//отображение игрового поля(клеток массива) и строки статуса, в которой будет написано количество ненулевых клеток.
+{
+
+    for (let i = 0; i < array.length; i++) {
+        fieldEl.innerHTML += "<div class=\"row\"></div>"
+    }
+
+    let rowsEl = document.querySelectorAll(".row");
+
+    for (let i = 0; i < array.length; i++) {
+        for (let a = 0; a < array.length; a++) {
+            rowsEl[i].innerHTML += "<div class=\"cell\"></div>";
+        }
+    }
+
+
+    fieldEl.innerHTML += "<div class=\"status-row\"></div>"
+    document.querySelector(".status-row").innerHTML = findFilledCells(array);
+}
+
+fillUpField(fieldEl, fieldArr);
+
+
+
+
+
