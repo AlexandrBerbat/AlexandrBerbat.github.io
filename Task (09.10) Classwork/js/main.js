fc5d7cc9cff2bbf9f4fad4802ccc4723d0a4fbab
Flexandr <aleksandrberbat@gmail.com>
2020-10-10 14:43:35 +0300

    task 09.10 classwork (10.10) buttons display func

--- Task (09.10) Classwork/js/main.js
+++ Task (09.10) Classwork/js/main.js
@@ -23,7 +23,7 @@ let fieldArr = createArray();
 function clearArray(array) {//очистка двумерного массива
     for (let i = 0; i < array.length; i++) {
         for (let a = 0; a < array.length; a++) {
-            array[i][a] = null;
+            array[i][a] = 0;
         }
     }
 }
@@ -73,29 +73,68 @@ function fillUpField(fieldEl, array)//отображение игрового п
 fillUpField(fieldEl, fieldArr);
 
 
-
-
-
-
-
-
 ////////////////////////////////////////6666666666666666666666///////////////////////////////////
 
 let allCellsEl = document.querySelectorAll(".cell");
 let pointer = [0, 0];
 
+let btnLeftEl = document.querySelector(".icon-left-big");
+let btnUpEl = document.querySelector(".icon-up-big");
+let btnDownEl = document.querySelector(".icon-down-big");
+let btnRightEl = document.querySelector(".icon-right-big");
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 //дефолт: первая клетка выделена, массив [1,0,0...]
 document.querySelector(".cell").classList.add("selected");
 fieldArr[pointer[0]][pointer[1]] = 1;
 document.querySelector(".status-row").innerHTML = findFilledCells(fieldArr);
+btnLeftEl.style.display = "none";
+btnUpEl.style.display = "none";
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-let btnLeftEl = document.querySelector(".icon-left-big");
-let btnUpEl = document.querySelector(".icon-up-big");
-let btnDownEl = document.querySelector(".icon-down-big");
-let btnRightEl = document.querySelector(".icon-right-big");
+
+////////////////888888888//////////////////
+function blockImpossibleWay(array, pointer) {
+    let resultWays = [];//0-left, 1-up, 2-right, 3-down// true - possible way, false - impossible way
+
+    if ((pointer[0] - 1) < 0)// || array[pointer[1] - 1][pointer[0]] == 2) 
+    {
+        resultWays[0] = false;//left
+    }
+    else {
+        resultWays[0] = true;
+
+    }
+    if ((pointer[0] + 1) == fieldLength)// || array[pointer[1] + 1][pointer[0]] == 2) 
+    {
+        resultWays[2] = false;//right
+    }
+    else {
+        resultWays[2] = true;
+
+    }
+    if ((pointer[1] - 1) < 0)// || array[pointer[1]][pointer[0] - 1] == 2) 
+    {
+        resultWays[1] = false;//up
+    }
+    else {
+        resultWays[1] = true;
+
+    }
+    if ((pointer[1] + 1) == fieldLength)// || array[pointer[1]][pointer[0] + 1] == 2) 
+    {
+        resultWays[3] = false;//down
+    }
+    else {
+        resultWays[3] = true;
+
+    }
+    return resultWays;
+};
+//////////////////////8888888//////////////////
+
 
 function refreshData(pointer) {
     let tempArr = trackPassedCells(fieldArr);
@@ -123,29 +162,69 @@ function refreshData(pointer) {
     console.table(fieldArr);
 
 
+    console.log(blockImpossibleWay(fieldArr, pointer));
+
+    let tempShit = blockImpossibleWay(fieldArr, pointer);
+
+    if (!tempShit[0]) {
+        btnLeftEl.style.display = "none";
+    }
+    else {
+        btnLeftEl.style.display = "flex";
+    }
+
+    if (!tempShit[1]) {
+        btnUpEl.style.display = "none";
+    }
+    else {
+        btnUpEl.style.display = "flex";
+    }
+
+    if (!tempShit[2]) {
+        btnRightEl.style.display = "none";
+    }
+    else {
+        btnRightEl.style.display = "flex";
+    }
+
+    if (!tempShit[3]) {
+        btnDownEl.style.display = "none";
+    }
+    else {
+        btnDownEl.style.display = "flex";
+    }
+
 
     document.querySelector(".status-row").innerHTML = findFilledCells(fieldArr);
+
+
 }
 
 function moveLeft() {
+
     if (pointer[0] > 0) {
         pointer[0] -= 1;
         refreshData(pointer);
     }
+
 }
 function moveDown() {
+
     if (pointer[1] < fieldLength - 1) {
         pointer[1] += 1;
         refreshData(pointer);
     }
+
 }
 function moveUp() {
+
     if (pointer[1] > 0) {
         pointer[1] -= 1;
         refreshData(pointer);
     }
 }
 function moveRight() {
+
     if (pointer[0] < fieldLength - 1) {
         pointer[0] += 1;
         refreshData(pointer);
@@ -180,7 +259,7 @@ document.addEventListener("keydown", (event) => {
 ////////////////////////////////////77777777777777777777777777///////////////////////////////
 
 
-function trackPassedCells(array) {
+function trackPassedCells(array) {//отмечаем какие клетки мы проходили во временном двумерном массиве
     let resultArr = new Array(fieldLength);
 
     for (let i = 0; i < resultArr.length; i++) {
@@ -201,6 +280,30 @@ function trackPassedCells(array) {
 
 
 
+
+
+
+
+
+console.log(blockImpossibleWay(fieldArr, pointer));
+
+
+///////////////////////////888888888888888888////////////////////////////////////////////
+
+// function blockImpossibleWay(array, pointer)
+// {
+//     if((pointer[0]-1) <= 0 || array[pointer[0]-1][0] == 2)
+//     {
+//         btnLeftEl.classList.add("disabl");
+//         console.log("allooo");
+//     }
+//     if((pointer[0]+1) >= fieldLength-1 || array[pointer[0]+1][0] == 2)
+//     {
+//         btnRightEl.classList.add("disabl");
+//     }
+// };
+
+// blockImpossibleWay(fieldArr, pointer);
 
 
 
