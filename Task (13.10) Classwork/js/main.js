604e613ce88a97f0d934a18072ca0c9d27dbbb25
Flexandr <aleksandrberbat@gmail.com>
2020-10-13 19:48:47 +0300

    task 13.10 classwork

---
+++ Task (13.10) Classwork/js/main.js
@@ -0,0 +1,37 @@
+let btn1El = document.querySelector(".btn1");
+let btn2El = document.querySelector(".btn2");
+let helloEl = document.querySelector(".hello");
+
+function btn1Active() {
+
+
+    if (btn2El.className.includes("disabl")) {
+        btn2El.classList.remove("disabl");
+    }
+    else {
+        btn2El.classList.add("disabl");
+        if (!helloEl.className.includes("disabl")) {
+            helloEl.classList.add("disabl");
+        }
+    }
+}
+
+function btn2Active() {
+
+    if(helloEl.className.includes("disabl"))
+    {
+        helloEl.classList.remove("disabl");
+    }
+    else
+    {
+
+        helloEl.classList.add("disabl");
+
+    }
+
+}
+
+
+
+btn1El.addEventListener("click", btn1Active);
+btn2El.addEventListener("click", btn2Active);
