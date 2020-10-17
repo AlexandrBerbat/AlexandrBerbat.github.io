57d71e01c227002e195cf4203579ee2d0f36795b
Flexandr <aleksandrberbat@gmail.com>
2020-10-17 14:13:31 +0300

    task 17.10 classwork

--- Task (13.10) Classwork/js/main.js
+++ Task (13.10) Classwork/js/main.js
@@ -9,51 +9,70 @@ const SPACES = 10;
 let parking = [];
 
 for (let i = 0; i < SPACES; i++) {
-
-
-
-    parking[i] = { id: i, occupied: false, time: "", };
+    parking[i] = { id: i + 1, occupied: false, time: "", };
 }
 
 let parkingLotsEl = document.querySelector(".parkingLots");
 
-for (let i = 0; i < SPACES; i++) {
-    parkingLotsEl.innerHTML += "<div class=\"parkingPlace\"></div>";
-}
-
-let parkingPlacesEl = document.querySelectorAll(".parkingPlace");
-
-for (let i = 0; i < parkingPlacesEl.length; i++) {
-    parkingPlacesEl[i].innerHTML =
-        `ID: ${parking[i].id}
-    Occupied: ${parking[i].occupied}
-    Time: ${parking[i].time}`;
-}
-
 function findLots(parkingArr)//свободные\занятые парковочные места
 {
     let result = [0, 0];
-    for (let i = 0; i < SPACES; i++) {
-        if (parkingArr[i].occupied == false) {
+
+    parkingArr.forEach((item, index) => {
+        if (parkingArr[index].occupied == false) {
             result[0]++;
         }
         else {
             result[1]++;
         }
-    }
+    })
 
     return result;//result[0] - свободные result[1] - заняты
 }
 
-let lots = findLots(parking);
-console.log(`Free: ${lots[0]}`);
-console.log(`Occupied: ${lots[1]}`);
+// function generateParkingPage(parkingArr) {
+
+//     parkingLotsEl.innerHTML = "";
+//     for (let i = 0; i < SPACES; i++) {
+
+//         parkingLotsEl.innerHTML += `<div class=\"parkingPlace\" id=\"${i}\"></div>`;
+//     }
+
+//     let parkingPlacesEl = document.querySelectorAll(".parkingPlace");
+
+//     for (let i = 0; i < parkingPlacesEl.length; i++) {
+//         parkingPlacesEl[i].innerHTML =
+//             `ID: ${parkingArr[i].id} <br>
+//             Occupied: ${parkingArr[i].occupied} <br>
+//             Time: ${parkingArr[i].time}`;
+//     }
+
+//     let occupiedLotsEl = document.querySelector(".occupiedPlaces");
+
+//     occupiedLotsEl.innerHTML = `Empty places: ${findLots(parkingArr)[0]} <br>Occupied places: ${findLots(parkingArr)[1]}`;
+// }
+
+// generateParkingPage(parking);
+
+parkingLotsEl.innerHTML = "";
+for (let i = 0; i < SPACES; i++) {
+
+    parkingLotsEl.innerHTML += `<div class=\"parkingPlace\" id=\"${i}\"></div>`;
+}
+
+let parkingPlacesEl = document.querySelectorAll(".parkingPlace");
+
+
+
+//////////////////////////////////
+
+
 
 let currTimeEl = document.querySelector(".currTime");
 
 function startTime() {
     let currTime = new Date();
-    
+
     currTimeEl.innerHTML = `${currTime.getHours()}:${currTime.getMinutes()}:${currTime.getSeconds()}`;
     let temp = setTimeout(startTime(), 1000);
 }
@@ -63,4 +82,54 @@ let timeId = setTimeout(function tick() {
     currTimeEl.innerHTML = `${currTime.getHours()}:${currTime.getMinutes()}:${currTime.getSeconds()}`;
 
     timeId = setTimeout(tick, 1000);
-  }, 1000);
+}, 1000);
+
+
+
+let parkingPlacesEls = document.querySelectorAll(".parkingPlace");
+
+
+function lotPick(ev) {
+    // console.dir(el.target);
+    console.dir(ev.target.id);
+    parking[ev.target.id].occupied = !parking[ev.target.id].occupied;
+
+
+    generateParkingPage(parking);
+}
+
+
+const placeClick = () => {
+    parking.forEach((item, index) => {
+
+    parkingPlacesEls[index].addEventListener("click", lotPick);
+})};
+
+
+
+
+
+function generateParkingPage(parkingArr) {
+
+        parkingLotsEl.innerHTML = "";
+        for (let i = 0; i < SPACES; i++) {
+    
+            parkingLotsEl.innerHTML += `<div class=\"parkingPlace\" id=\"${i}\"></div>`;
+        }
+    
+        let parkingPlacesEl = document.querySelectorAll(".parkingPlace");
+    
+        for (let i = 0; i < parkingPlacesEl.length; i++) {
+            parkingPlacesEl[i].innerHTML =
+                `ID: ${parkingArr[i].id} <br>
+                Occupied: ${parkingArr[i].occupied} <br>
+                Time: ${parkingArr[i].time}`;
+        }
+    
+        let occupiedLotsEl = document.querySelector(".occupiedPlaces");
+    
+        occupiedLotsEl.innerHTML = `Empty places: ${findLots(parkingArr)[0]} <br>Occupied places: ${findLots(parkingArr)[1]}`;
+        placeClick();
+    }
+    
+    generateParkingPage(parking);
