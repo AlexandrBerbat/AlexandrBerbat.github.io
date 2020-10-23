f02d1cd23655e37ec3d9ada70341d6d3b905154d
Flexandr <aleksandrberbat@gmail.com>
2020-10-23 20:44:17 +0300

    task 23.10 classwork

--- Task (20.10) Classwork/js/main.js
+++ Task (20.10) Classwork/js/main.js
@@ -1,38 +1,71 @@
+
+
+
+
+// const btnFunc = (el1, el2, el3, el4, el5) => {
+//     console.log(el1.id);
+//     setTimeout(() => {
+//         el1.classList.add("done");
+//         setTimeout(() => {
+//             el1.innerHTML = el1.id;
+//             grayEls[0].classList.add("show");
+//             el2.classList.add("done");
+//             setTimeout(() => {
+//                 el2.innerHTML = el2.id;
+//                 grayEls[1].classList.add("show");
+//                 el3.classList.add("done");
+//                 setTimeout(() => {
+//                     el3.innerHTML = el3.id;
+//                     grayEls[2].classList.add("show");
+//                     el4.classList.add("done");
+//                     setTimeout(() => {
+//                         el4.innerHTML = el4.id;
+//                         grayEls[3].classList.add("show");
+//                         el5.classList.add("done");
+//                         setTimeout(() => {
+//                             el5.innerHTML = el5.id;
+//                             grayEls[4].classList.add("show");
+//                         },3000);
+
+//                     }, 3000);
+//                 }, 3000);
+//             }, 3000);
+//         }, 3000);
+//     }, 3000);
+// }
+
 let btnEls = document.querySelectorAll(".btn");
 let grayEls = document.querySelectorAll(".gray-div");
 
+const btnFunc = (BtnEls, GrayEls) => {
+    let iter = 0;
+    const BLOCKS = 3;
 
+    let intervalID = setInterval(() => {
+        console.log("iter: " + iter);
 
-const btnFunc = (el1, el2, el3, el4, el5) => {
-    console.log(el1.id);
-    setTimeout(() => {
-        el1.classList.add("done");
-        setTimeout(() => {
-            el1.innerHTML = el1.id;
-            grayEls[0].classList.add("show");
-            el2.classList.add("done");
-            setTimeout(() => {
-                el2.innerHTML = el2.id;
-                grayEls[1].classList.add("show");
-                el3.classList.add("done");
-                setTimeout(() => {
-                    el3.innerHTML = el3.id;
-                    grayEls[2].classList.add("show");
-                    el4.classList.add("done");
-                    setTimeout(() => {
-                        el4.innerHTML = el4.id;
-                        grayEls[3].classList.add("show");
-                        el5.classList.add("done");
-                        setTimeout(() => {
-                            el5.innerHTML = el5.id;
-                            grayEls[4].classList.add("show");
-                        },3000);
+        if (iter < BLOCKS) {
+            BtnEls[iter].classList.add("done");
+            
+        }
+        if(BtnEls[iter-1])
+        {
+            BtnEls[iter-1].innerHTML = `${BtnEls[iter-1].id}`;
+            BtnEls[iter-1].classList.add("end");
+        }
 
-                    }, 3000);
-                }, 3000);
-            }, 3000);
-        }, 3000);
-    }, 3000);
+        iter++;
+        iter >= 4 ? clearInterval(intervalID) : true;
+    }, 3213);
+
 }
 
-btnFunc(btnEls[0],btnEls[1],btnEls[2],btnEls[3],btnEls[4])
+btnFunc(btnEls, grayEls);
+
+for (let i = 0; i < btnEls.length; i++) {
+    btnEls[i].addEventListener("click", () => {
+        if (btnEls[i].className.includes("end")) {
+            grayEls[i].classList.toggle("show");
+        }
+    })
+}
