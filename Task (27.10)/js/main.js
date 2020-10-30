158f6cc65378ee604461aa31ed3adff3257be28b
Flexandr <aleksandrberbat@gmail.com>
2020-10-30 16:56:40 +0200

    task 30.10 start

--- Task (27.10)/js/main.js
+++ Task (27.10)/js/main.js
@@ -1,32 +1,49 @@
-let request = require('request');
-let URL = 'https://dou.ua/';
+// let request = require('request');
+let URL = 'https://google.com/';
 
-let results = [];
-const searchStr = `<img class="img" loading="lazy" src="`;
+// let results = [];
+// const searchStr = `<img class="img" loading="lazy" src="`;
 
-request(URL, function (err, res, body) {
-    
-    let tempIndex = [];
-    let q = 1;
-    tempIndex[0] = body.indexOf(searchStr); 
-    let prevIndex = tempIndex[0];
+// request(URL, function (err, res, body) {
 
-    do
-    {
-        tempIndex[q] = body.indexOf(searchStr, prevIndex+1);
-        prevIndex = tempIndex[q];        
-        q += 1;
-    }while(body.indexOf(searchStr,prevIndex+1) !== -1);
+//     let tempIndex = [];
+//     let q = 1;
+//     tempIndex[0] = body.indexOf(searchStr);
+//     let prevIndex = tempIndex[0];
 
-    
-    for(let i = 0; i < tempIndex.length; i++)
-    {
-        let tempStartConcat = tempIndex[i]+searchStr.length;
-        let tempEndConcat =  body.indexOf("\"",tempStartConcat+1);
-        results[i] = body.substr(tempStartConcat, tempEndConcat-tempStartConcat);
+//     do {
+//         tempIndex[q] = body.indexOf(searchStr, prevIndex + 1);
+//         prevIndex = tempIndex[q];
+//         q += 1;
+//     } while (body.indexOf(searchStr, prevIndex + 1) !== -1);
 
-        console.log(results[i]);
-    }
 
+//     for (let i = 0; i < tempIndex.length; i++) {
+//         let tempStartConcat = tempIndex[i] + searchStr.length;
+//         let tempEndConcat = body.indexOf("\"", tempStartConcat + 1);
+//         results[i] = body.substr(tempStartConcat, tempEndConcat - tempStartConcat);
+
+//         console.log(results[i]);
+//     }
+
+// });
+
+const fs = require('fs');
+
+const str = "hello!";
+const file = "1.txt";
+let content = null;
+
+
+
+
+fs.writeFile(file,str,(err) => {
+    console.log('err', err);
+
+    const a = fs.readFile(file, 'utf8', (err,data) => {
+        console.log('err', err);
+        console.log(data);
+    });
 });
+
 
