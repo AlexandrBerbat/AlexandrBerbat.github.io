6566719f40e116783bf618febb605e94443c8d52
Flexandr <aleksandrberbat@gmail.com>
2020-10-31 11:05:29 +0200

    task 31.10 classwork start

--- Task (30.10)/js/main.js
+++ Task (30.10)/js/main.js
@@ -1,27 +1,7 @@
-const fs = require('fs');
+const fs = require("fs");
 
-
-const file = "testFile.txt";
-// const fileName = "testFile.txt";
 const startPath = "./test1/testFile.txt";
 const endPath = "./test2/testFile.txt";
-
-// fs.writeFile(file, str, (err) => {
-
-//     !err ? console.log('err', err) : false;
-
-//     const a = fs.readFile(file, 'utf8', (err, data) => {
-
-//         if (!err) {
-//             console.log('err', err)
-//         } else {
-
-//             console.log("Файл успешно перемещен");
-//             console.log(data);
-
-//         }
-//     });
-// });
 
 
 const a = fs.readFile(startPath, 'utf8', (err, data) => {
@@ -29,6 +9,7 @@ const a = fs.readFile(startPath, 'utf8', (err, data) => {
     if (err) {
         console.log('err', err)
     } else {
+
         const b = fs.writeFile(endPath, data, (err, data) => {
 
             if (err) {
@@ -44,6 +25,7 @@ const a = fs.readFile(startPath, 'utf8', (err, data) => {
 
             }
         });
+
 
     }
 });
