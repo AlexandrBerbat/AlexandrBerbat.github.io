515f349c890a9cf00325e6e462030a9f5717474c
Flexandr <aleksandrberbat@gmail.com>
2020-12-05 13:21:35 +0200

    task 05.12 classwork

--- Task05.12Classwork/task/routes/index.js
+++ Task05.12Classwork/task/routes/index.js
@@ -5,65 +5,64 @@ var upload = multer();
 var axios = require('axios');
 
 
+function CharacterInFilm(charID, allChars) {
+  let reg = /\d/gi;
+  allChars = allChars.map(item => {
+    item = item.match(reg);
+    item = item.join('');
+    return item
+  })
+
+  if (allChars.includes(charID)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', upload.none(), (req,res) => {
+router.post('/', upload.none(), (req, res) => {
 
   let data = req.body;
-  let page = {filmName: "", filmID : data.film_id, characterName: "", characterID: data.character_id, WasInTheFilm: false};
+  let page = { filmName: "", filmID: data.film_id, characterName: "", characterID: data.character_id, WasInTheFilm: false };
 
+  let promiseFilm = axios.get(`https://swapi.dev/api/films/${data.film_id}/`);
+  let promiseChar = axios.get(`https://swapi.dev/api/people/${page.characterID}/`);
 
-  axios.get(`https://swapi.dev/api/films/${data.film_id}/`)
-  .then(res1 => {
-    let tempData = res1.data;
-
-    page.filmName = tempData.title;
-
-    let reg = /\d/gi;
-    tempData.characters = tempData.characters.map(item => {
-      item = item.match(reg);
-      item = item.join('');
-      return item
-    })
-
-    if(tempData.characters.includes(data.character_id))
-    {
-      console.log('Персонаж присутствовал в фильме');
-      page.WasInTheFilm = true;
-
-    }
-    return page;
-
-  })
-  .catch(err => {
-    console.log(`Error: ${err}`);
-  })
-  .then((page) => {
-    axios.get(`https://swapi.dev/api/people/${page.characterID}/`)
-    .then(res2 => {
-      page.characterName = res2.data.name;
+  Promise.all([promiseFilm, promiseChar])
+    .then(values => {
+      page.WasInTheFilm = CharacterInFilm(page.characterID, values[0].data.characters)
+      page.filmName = values[0].data.title;
+      page.characterName = values[1].data.name;
       return page;
     })
     .catch(err => {
       console.log(`Error: ${err}`);
     })
     .then(page => {
-      if(page.WasInTheFilm)
-      {
+      if (page.WasInTheFilm) {
         res.send(`Character ${page.characterName} was present in ${page.filmName}`);
         res.end();
       }
-      else{
+      else {
         res.send(`Character ${page.characterName} wasn\`t present in ${page.filmName}`);
         res.end();
       }
     })
 
-  })
-
 })
+
+
+
+
+
+
+
+
 
 module.exports = router;
