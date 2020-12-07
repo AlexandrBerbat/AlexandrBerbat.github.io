2698a7f85376433339f08099b27ebd81e877931f
Flexandr <aleksandrberbat@gmail.com>
2020-12-07 17:33:34 +0200

    tassk 08.12 v0.1

--- Task07.12/task/routes/index.js
+++ Task07.12/task/routes/index.js
@@ -15,32 +15,54 @@ router.get('/', function (req, res, next) {
 
 router.get('/checkJson', function (req, res, next) {
   fs.readFile(fileName, "utf-8")
-  .then(values => {
-    res.send(values);
-  })
+    .then(values => {
+      res.send(values);
+    })
 })
 
 router.post('/', upload.none(), (req, res) => {
 
   let data = req.body;
 
+
   fs.readFile(fileName, "utf-8")
     .then((values) => {
+      console.log(values);
       if (values == "") {
-        let writeStr = `${startFile}{'author': ${data.author}, 'date': ${data.date}, 'article': ${data.article}, 'tags': ${data.tags}, 'comments': ${data.comments}}${endFile}`;
+        let json = JSON.stringify(data);
+
+        let writeStr = `${startFile}${json}${endFile}`;
+
         fs.writeFile(fileName, writeStr, "utf-8")
+          .catch(err => {
+            res.send(`Ошибка записи файла: ${err}`);
+            res.end();
+          })
+          .then(() => {
+            res.send(`Статья успешно записана`)
+            res.end();
+          })
       }
       else {
-        let tempData = values.slice(0,-1);
-        let writeStr = `,{'author': ${data.author}, 'date': ${data.date}, 'article': ${data.article}, 'tags': ${data.tags}, 'comments': ${data.comments}}${endFile}`;
-        fs.writeFile(fileName, `${tempData}${writeStr}`, "utf-8");
+        let json = JSON.stringify(data);
+        let tempData = values.slice(0, -1);
+
+        let writeStr = `,${json}${endFile}`;
+
+        fs.writeFile(fileName, `${tempData}${writeStr}`, "utf-8")
+          .catch(err => {
+            res.send(`Ошибка записи файла: ${err}`);
+            res.end();
+          })
+          .then(() => {
+            res.send(`Статья успешно записана`)
+            res.end();
+          })
+
       }
     })
-    .catch(err=> {
-      console.log(`Error: ${err}`);
-    })
-    .then(()=> {
-      res.send(`Статья успешно записана`)
+    .catch(err => {
+      res.send(`Ошибка чтения файла: ${err}`);
       res.end();
     })
 
