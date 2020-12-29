c317f5919a1ade01a1ebdd5495a5ede389571daf
Flexandr <aleksandrberbat@gmail.com>
2020-12-29 16:17:28 +0200

    task29.12v0.1

--- Task29.12/test.js
+++ Task29.12/test.js
@@ -1,9 +1,16 @@
-  const fs = require('fs');
+const fs = require('fs');
 
-const start = '3/25/2019';
-const end = '12/25/2020';
+function randomInteger(min, max) {
+    // получить случайное число от (min-0.5) до (max+0.5)
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return rand;
+}
 
-//'MM:DD:YYYY' - input;
+
+const start = '2019-3-25';
+const end = '2020-12-25';
+
+//'YYYY-MM-DD' - input (любой разделитель)
 //'YYYY-MM-DD HH:MM:SS' - output
 
 function generateDate(startDateStr, endDateStr, numberOfGenerations) {
@@ -13,10 +20,9 @@ function generateDate(startDateStr, endDateStr, numberOfGenerations) {
     const options = { weekday: 'narrow', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
 
     let result = [];
-    
 
     for (let i = 0; i < numberOfGenerations; i++) {
-        let temp = new Date(+startDate + Math.random() * (endDate - startDate)).toLocaleDateString('eu', options).split(" ");
+        let temp = new Date(+startDate + randomInteger(0.5, 0.5) * (endDate - startDate)).toLocaleDateString('ru', options).split(" ");
         result[i] = `${temp[0]}-${temp[1]}-${temp[2].slice(0, -1)} ${temp[4]}`;
     }
 
@@ -25,10 +31,12 @@ function generateDate(startDateStr, endDateStr, numberOfGenerations) {
 
 let tempRes = generateDate(start, end, 100);
 
-fs.writeFile("test.json", tempRes, function(error){
-    if(error) throw error;
-    console.log("Запись файла завершена.");
-});
+console.log(tempRes);
+
+// fs.writeFile("test.json", tempRes, function(error){
+//     if(error) throw error;
+//     console.log("Запись файла завершена.");
+// });
 
 
 
